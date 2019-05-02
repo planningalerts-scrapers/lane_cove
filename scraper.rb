8be269501b5ac59b653b4d4ebd5c7962852c27db
Matthew Landauer <mlandauer@gmail.com>
2019-05-02 17:13:06 +1000

    Always save record as planningalerts now does versioning

--- scraper.rb
+++ scraper.rb
@@ -34,12 +34,8 @@ if not fullcontent.nil? and not fullcontent.search('div')[5].nil?
       }
         
       #p record
-      if ScraperWiki.select("* from data where `council_reference`='#{record['council_reference']}'").empty? 
-        puts "Saving new record " + record['council_reference']
-        ScraperWiki.save_sqlite(['council_reference'], record)
-      else
-        puts "Skipping already saved record " + record['council_reference']
-      end
+      puts "Saving new record " + record['council_reference']
+      ScraperWiki.save_sqlite(['council_reference'], record)
     else
       raise "Unexpected type: #{block.name} #{block}"
     end
